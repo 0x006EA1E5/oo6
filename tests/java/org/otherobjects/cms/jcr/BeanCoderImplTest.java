ecd8571ce5c4f2b5a8ec380d940dabdcbe488a99
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-06-13 13:38:14 +0000

    Initial commit
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%4014

---
+++ tests/java/org/otherobjects/cms/jcr/BeanCoderImplTest.java
@@ -0,0 +1,20 @@
+package org.otherobjects.cms.jcr;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class BeanCoderImplTest
+{
+    @Test
+    public void testConvertFromNode()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testConvertToNode()
+    {
+        fail("Not yet implemented");
+    }
+}
