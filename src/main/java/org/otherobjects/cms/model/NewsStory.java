73143033afd322ed4c7ff435d10cb901178af30f
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-06-14 12:04:10 +0000

    Reorg
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%4019

---
+++ src/main/java/org/otherobjects/cms/model/NewsStory.java
@@ -0,0 +1,39 @@
+package org.otherobjects.cms.model;
+
+public class NewsStory
+{
+    private String path;
+    private String title;
+    private String cotent;
+
+    public String getPath()
+    {
+        return path;
+    }
+
+    public void setPath(String path)
+    {
+        this.path = path;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getCotent()
+    {
+        return cotent;
+    }
+
+    public void setCotent(String cotent)
+    {
+        this.cotent = cotent;
+    }
+
+}
