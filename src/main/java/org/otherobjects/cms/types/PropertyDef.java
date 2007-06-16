05bebeb5e8977e85d08c3c4bca4e6af66d1ff5a8
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-06-16 09:30:50 +0000

    Updates
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%4031

--- src/main/java/org/otherobjects/cms/types/PropertyDef.java
+++ src/main/java/org/otherobjects/cms/types/PropertyDef.java
@@ -1,10 +1,32 @@
 package org.otherobjects.cms.types;
 
+/**
+ * Defines a property of a type.
+ * 
+ * <p>Currently supported simple property types are:
+ * 
+ * <ul>
+ * <li>string (String.class)
+ * <li>text (String.class)
+ * <li>date (Date.class)
+ * <li>time (Date.class)
+ * <li>timestamp (Date.class)
+ * <li>boolean (Boolean.class)
+ * <li>number (Long.class Integer.class)
+ * <li>decimal (BigDecimal.class, Float.class, Double.class) -- specify decimal places in format -- stored as double
+ * </ul>
+ * 
+ * @author rich
+ *
+ */
 public class PropertyDef
 {
     private String name;
     private String type;
     private String relatedType;
+    
+    /** To specify format for decimals. TODO Maybe others? */
+    private String format; 
     
     /** Human friendly name for property. Can be inferred from name */
     private String label;
@@ -15,6 +37,7 @@ public class PropertyDef
     /** Help text to assist choosing value for this property. */
     private String help;
 
+    
     public PropertyDef(String name, String propertyType, String relatedType)
     {
         setName(name);
