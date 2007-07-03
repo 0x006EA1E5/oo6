552f8595b145a189915df269c0f4dea1fa028e99
joerg@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-07-03 14:12:01 +0000

    ongoing acegi experiments
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40104

--- src/test/java/org/otherobjects/cms/dao/SecureObject1Dao.java
+++ src/test/java/org/otherobjects/cms/dao/SecureObject1Dao.java
@@ -3,5 +3,5 @@ package org.otherobjects.cms.dao;
 import org.otherobjects.cms.model.SecureObject1;
 
 public interface SecureObject1Dao extends GenericDao<SecureObject1, Long> {
-	public SecureObject1 get(Long id);
+	
 }
