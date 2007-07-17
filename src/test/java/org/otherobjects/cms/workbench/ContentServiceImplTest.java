a1cbfbac9a666a24f41b9f821fe6f17cbf582a3a
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-07-17 11:24:49 +0000

    Add tests
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40217

---
+++ src/test/java/org/otherobjects/cms/workbench/ContentServiceImplTest.java
@@ -0,0 +1,46 @@
+package org.otherobjects.cms.workbench;
+
+import org.otherobjects.cms.dao.DynaNodeDao;
+import org.otherobjects.cms.model.DynaNode;
+import org.otherobjects.cms.test.BaseJcrTestCase;
+
+public class ContentServiceImplTest extends BaseJcrTestCase
+{
+    // Injected by Spring
+    private DynaNodeDao dynaNodeDao;
+    private ContentServiceImpl contentService;
+
+    @Override
+    protected void onSetUp() throws Exception
+    {
+        super.onSetUp();
+        contentService = new ContentServiceImpl();
+        contentService.setDynaNodeDao(dynaNodeDao);
+    }
+
+    public void testTestDependencies()
+    {
+        // Create item in site root
+        assertNotNull(dynaNodeDao);
+        assertNotNull(contentService);
+    }
+
+    public DynaNodeDao getDynaNodeDao()
+    {
+        return dynaNodeDao;
+    }
+
+    public void setDynaNodeDao(DynaNodeDao dynaNodeDao)
+    {
+        this.dynaNodeDao = dynaNodeDao;
+    }
+
+    public void testCreateItem()
+    {
+        DynaNode siteRoot = dynaNodeDao.getByPath("/site/");
+        DynaNode new1 = contentService.createItem(siteRoot.getId(), "Article");
+        assertEquals("Untitled Article 1", new1.getLabel()); 
+        DynaNode new2 = contentService.createItem(siteRoot.getId(), "Article");
+        assertEquals("Untitled Article 2", new2.getLabel()); 
+    }
+}
