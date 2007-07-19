1456b7845b1addb88cc1cb2e3e80779e41bef6ab
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-07-19 13:55:43 +0000

    Update tests
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40251

--- src/test/java/org/otherobjects/cms/util/StringUtilsTest.java
+++ src/test/java/org/otherobjects/cms/util/StringUtilsTest.java
@@ -2,12 +2,15 @@ package org.otherobjects.cms.util;
 
 import junit.framework.TestCase;
 
-import org.otherobjects.cms.util.StringUtils;
-
 public class StringUtilsTest extends TestCase
 {
     public void testGenerateLabel()
     {
         assertEquals("News Story", StringUtils.generateLabel("newsStory"));
+    }
+
+    public void testGenerateUrlCode()
+    {
+        assertEquals("news-story-new", StringUtils.generateUrlCode("News Story. New."));
     }
 }
---
+++ src/test/java/org/otherobjects/cms/workbench/NavigatorServiceImplTest.java
@@ -0,0 +1,10 @@
+package org.otherobjects.cms.workbench;
+
+import junit.framework.TestCase;
+
+public class NavigatorServiceImplTest extends TestCase
+{
+    public void testGetRootItem()
+    {
+    }
+}
