6f710bbbf3901763ee76fdd17e456217c3a08af9
joerg@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-07-20 08:45:20 +0000

    Java5 style package info added
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40266

---
+++ src/main/java/org/otherobjects/cms/beans/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Classes in this package deal with 'beanifying' objects that might not be proper beans to start with 
+ * in order to use them in places where one needs proper beans (binding, validation etc.)
+ */
+package org.otherobjects.cms.beans;
