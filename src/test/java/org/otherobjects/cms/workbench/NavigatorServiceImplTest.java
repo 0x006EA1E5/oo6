6a02a46f2bd9bfdb4af6bdd8f76c195c6a4f9a7f
joerg@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-07-20 14:16:51 +0000

    tests added
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40288

--- src/test/java/org/otherobjects/cms/workbench/NavigatorServiceImplTest.java
+++ src/test/java/org/otherobjects/cms/workbench/NavigatorServiceImplTest.java
@@ -1,10 +1,118 @@
 package org.otherobjects.cms.workbench;
 
-import junit.framework.TestCase;
+import java.util.List;
 
-public class NavigatorServiceImplTest extends TestCase
+import org.otherobjects.cms.dao.DynaNodeDao;
+import org.otherobjects.cms.model.DynaNode;
+import org.otherobjects.cms.test.BaseJcrTestCase;
+
+public class NavigatorServiceImplTest extends BaseJcrTestCase
 {
+	private DynaNodeDao dynaNodeDao;
+    private NavigatorServiceImpl navigatorService;
+
+    @Override
+    protected void onSetUp() throws Exception
+    {
+        super.onSetUp();
+        navigatorService = new NavigatorServiceImpl();
+        navigatorService.setDynaNodeDao(dynaNodeDao);
+    }
+    
+    public void testTestDependencies()
+    {
+        // Create item in site root
+        assertNotNull(dynaNodeDao);
+        assertNotNull(navigatorService);
+    }
+    
     public void testGetRootItem()
     {
+    	DynaNode dynaNode = dynaNodeDao.getByPath("/site");
+    	assertNotNull(dynaNode);
     }
+    
+    public void testGetSubContainers()
+    {
+    	DynaNode root = dynaNodeDao.getByPath("/site");
+    	List<WorkbenchItem> items= navigatorService.getSubContainers(root.getId());
+    	assertTrue(items.size() > 0);
+    }
+    
+    public void testAddItem()
+    {
+    	DynaNode root = dynaNodeDao.getByPath("/site");
+    	WorkbenchItem item = navigatorService.addItem(root.getId(), "");
+    	System.out.println(item.getId() + " " + item.getLinkPath());
+    	assertTrue(item instanceof DynaNode);
+    	DynaNode dynaNode = (DynaNode) item;
+    	assertTrue(dynaNode.getPath().startsWith("/site"));
+    }
+    
+    public void testGetItemContents()
+    {
+    	DynaNode about = dynaNodeDao.getByPath("/site/about");
+    	assertTrue(about instanceof DynaNode);
+    	
+    	List<WorkbenchItem> items = navigatorService.getItemContents(about.getId());
+    	
+    	assertTrue(items.size() > 0);
+    	
+    	DynaNode article = null;
+    	for(WorkbenchItem wi: items)
+    	{
+    		assertTrue(wi instanceof DynaNode);
+    		DynaNode wiNode = (DynaNode) wi;
+    		if(wiNode.getOoType().equals("Article"))
+    		{
+    			article = wiNode;
+    			break;
+    		}
+    	}
+    	
+    	assertNotNull(article);
+    }
+    
+    public void testRenameItem()
+    {
+    	DynaNode root = dynaNodeDao.getByPath("/site");
+    	WorkbenchItem item = navigatorService.addItem(root.getId(), "");
+    	
+    	DynaNode newNode = (DynaNode) item;
+    	navigatorService.renameItem(newNode.getId(), "asdasdasdasdasd");
+    	
+    	System.out.println(newNode.getLabel());
+    	
+    	//assertTrue(newNode.getLabel().equals("asdasdasdasdasd"));
+    	
+    	
+    }
+    
+    public void testMoveItem()
+    {
+    	DynaNode root = dynaNodeDao.getByPath("/site");
+    	
+    	WorkbenchItem item = navigatorService.addItem(root.getId(), "");
+    	
+    	List<WorkbenchItem> wis = navigatorService.getSubContainers(root.getId());
+    	
+    	WorkbenchItem lastItem = wis.get(wis.size() -1);
+    	
+    	assertEquals(lastItem.getLabel(), item.getLabel());
+    	
+    	WorkbenchItem firstItem = wis.get(0);
+    	
+    	navigatorService.moveItem(lastItem.getId(), firstItem.getId(), "before");
+    	
+//    	List<WorkbenchItem> wisReordered = navigatorService.getSubContainers(root.getId());
+//    	
+//    	assertEquals(item.getLabel(), wisReordered.get(0).getLabel());
+    	
+    }
+
+	public void setDynaNodeDao(DynaNodeDao dynaNodeDao) {
+		this.dynaNodeDao = dynaNodeDao;
+	}
+    
+    
 }
