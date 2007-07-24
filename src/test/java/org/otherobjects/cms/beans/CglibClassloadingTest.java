f5d325e22fffca916f6646cb7ebe9c36cc27018f
joerg@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-07-24 05:51:12 +0000

    improved
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40298

--- src/test/java/org/otherobjects/cms/beans/CglibClassloadingTest.java
+++ src/test/java/org/otherobjects/cms/beans/CglibClassloadingTest.java
@@ -1,22 +1,44 @@
 package org.otherobjects.cms.beans;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
 
 import net.sf.cglib.beans.BeanGenerator;
 
+import org.apache.commons.beanutils.PropertyUtils;
 import org.otherobjects.cms.model.DynaNode;
 
 import junit.framework.TestCase;
 
 public class CglibClassloadingTest extends TestCase {
 	
-	public void testTwoBeansSameClassname()
+	public void testTwoBeansSameClassname() 
 	{
 		Object bean1 = getDynaNodeBean();
 		Object bean2 = getDynaNodeBean();
 		
-		System.out.println("Name: " + bean1.getClass().getName());
+		
+		String genClassName = bean1.getClass().getName();
+		System.out.println("Name: " + genClassName);
 		assertEquals(bean1.getClass().getName(), bean2.getClass().getName());
+		
+		
+		try {
+			Class clazz = Class.forName(genClassName);
+			Object bean3 = clazz.newInstance();
+			PropertyUtils.setSimpleProperty(bean3, "testString", "This is a test");
+		} catch (ClassNotFoundException e) {
+			fail("We should find this class as we just added it to the classloader");
+		} catch (InstantiationException e) {
+			fail();
+		} catch (IllegalAccessException e) {
+			fail();
+		} catch (InvocationTargetException e) {
+			fail();
+		} catch (NoSuchMethodException e) {
+			fail();
+		}
+		
 	
 	}
 	
