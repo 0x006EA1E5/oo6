eb1b4f942c3a6e7f3c9c04de9b469180c16a1a6a
joerg@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-07-24 07:05:37 +0000

    resurrected TypeServiceMapper
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40299

---
+++ src/main/java/org/otherobjects/cms/jcr/TypeServiceMapperImpl.java
@@ -0,0 +1,177 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.otherobjects.cms.jcr;
+
+import org.apache.jackrabbit.ocm.exception.IncorrectPersistentClassException;
+import org.apache.jackrabbit.ocm.exception.InitMapperException;
+import org.apache.jackrabbit.ocm.manager.beanconverter.impl.DefaultBeanConverterImpl;
+import org.apache.jackrabbit.ocm.manager.beanconverter.impl.ReferenceBeanConverterImpl;
+import org.apache.jackrabbit.ocm.mapper.Mapper;
+import org.apache.jackrabbit.ocm.mapper.model.BeanDescriptor;
+import org.apache.jackrabbit.ocm.mapper.model.ClassDescriptor;
+import org.apache.jackrabbit.ocm.mapper.model.FieldDescriptor;
+import org.apache.jackrabbit.ocm.mapper.model.MappingDescriptor;
+import org.otherobjects.cms.model.CmsNode;
+import org.otherobjects.cms.types.JcrTypeServiceImpl;
+import org.otherobjects.cms.types.PropertyDef;
+import org.otherobjects.cms.types.TypeDef;
+import org.otherobjects.cms.types.TypeService;
+
+public class TypeServiceMapperImpl implements Mapper
+{
+    private JcrTypeServiceImpl typeService;
+
+    private MappingDescriptor mappingDescriptor;
+
+    /**
+     * No-arg constructor.
+     */
+    public TypeServiceMapperImpl()
+    {
+    }
+
+    public TypeServiceMapperImpl(JcrTypeServiceImpl typeService)
+    {
+        this.typeService = typeService;
+        buildMapper();
+    }
+
+    protected Mapper buildMapper()
+    {
+        if (typeService != null)
+        {
+            this.mappingDescriptor = new MappingDescriptor();
+
+            for (TypeDef typeDef : typeService.getTypes())
+            {
+                ClassDescriptor classDescriptor = createClassDescriptor(typeDef);
+                mappingDescriptor.addClassDescriptor(classDescriptor);
+            }
+
+            this.mappingDescriptor.setMapper(this);
+        }
+        else
+        {
+            throw new InitMapperException("No mappings were provided");
+        }
+
+        return this;
+    }
+
+    protected ClassDescriptor createClassDescriptor(TypeDef typeDef)
+    {
+        ClassDescriptor cd = new ClassDescriptor();
+        if (typeDef.getClassName() != null)
+            cd.setClassName(typeDef.getName());
+        else
+            cd.setClassName(CmsNode.class.getName());
+        cd.setJcrNodeType("oo:node");
+
+        // Add standard properties
+        FieldDescriptor fd = new FieldDescriptor();
+        fd.setFieldName("id");
+        fd.setJcrName("id");
+        fd.setUuid(true);
+        cd.addFieldDescriptor(fd);
+
+        FieldDescriptor fd2 = new FieldDescriptor();
+        fd2.setFieldName("jcrPath");
+        fd2.setJcrName("jcrPath");
+        fd2.setPath(true);
+        cd.addFieldDescriptor(fd2);
+
+        FieldDescriptor fd3 = new FieldDescriptor();
+        fd3.setFieldName("label");
+        fd3.setJcrName("label");
+        cd.addFieldDescriptor(fd3);
+
+//        FieldDescriptor fd4 = new FieldDescriptor();
+//        fd4.setFieldName("description");
+//        fd4.setJcrName("description");
+//        cd.addFieldDescriptor(fd4);
+
+        // Add custom properties
+        for (PropertyDef propDef : typeDef.getProperties())
+        {
+            String propertyType = propDef.getType();
+            if (propertyType.equals("component"))
+            {
+                BeanDescriptor bd = new BeanDescriptor();
+                bd.setFieldName("data." + propDef.getName());
+                bd.setJcrName(propDef.getName());
+                bd.setConverter(DefaultBeanConverterImpl.class.getName());
+                cd.addBeanDescriptor(bd);
+            }
+            else if (propertyType.equals("reference"))
+            {
+                BeanDescriptor bd = new BeanDescriptor();
+                bd.setFieldName("data." + propDef.getName());
+                bd.setJcrName(propDef.getName());
+                bd.setConverter(ReferenceBeanConverterImpl.class.getName());
+                cd.addBeanDescriptor(bd);
+            }
+            else
+            {
+                FieldDescriptor f = new FieldDescriptor();
+                f.setFieldName("data." + propDef.getName());
+                f.setJcrName(propDef.getName());
+                f.setConverter(typeService.getJcrConverter(propertyType).getClass().getName());
+                cd.addFieldDescriptor(f);
+            }
+        }
+        return cd;
+    }
+
+    /**
+    *
+    * @see org.apache.jackrabbit.ocm.mapper.Mapper#getClassDescriptorByClass(java.lang.Class)
+    */
+    @SuppressWarnings("unchecked")
+    public ClassDescriptor getClassDescriptorByClass(Class clazz)
+    {
+        ClassDescriptor descriptor = mappingDescriptor.getClassDescriptorByName(clazz.getName());
+        if (descriptor == null)
+        {
+            throw new IncorrectPersistentClassException("Class of type: " + clazz.getName() + " has no descriptor.");
+        }
+        return descriptor;
+    }
+
+    /**
+    * @see org.apache.jackrabbit.ocm.mapper.Mapper#getClassDescriptorByNodeType(String)
+    */
+    public ClassDescriptor getClassDescriptorByNodeType(String jcrNodeType)
+    {
+        ClassDescriptor descriptor = mappingDescriptor.getClassDescriptorByNodeType(jcrNodeType);
+        if (descriptor == null)
+        {
+            throw new IncorrectPersistentClassException("Node type: " + jcrNodeType + " has no descriptor.");
+        }
+        return descriptor;
+    }
+
+    public TypeService getTypeService()
+    {
+        return typeService;
+    }
+
+    public void setTypeService(JcrTypeServiceImpl typeService)
+    {
+        this.typeService = typeService;
+    }
+
+}
