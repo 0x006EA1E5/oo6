7c1f2dbf23b0f8546289806051ffe1b648db3c4b
joerg@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-08-14 11:29:21 +0000

    hibernate search stuff postponed
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40401

--- src/test/java/org/otherobjects/cms/hibernate/IndexedPersistenObjectTest.java
+++ src/test/java/org/otherobjects/cms/hibernate/IndexedPersistenObjectTest.java
@@ -20,15 +20,15 @@ public class IndexedPersistenObjectTest extends BaseDaoTestCase {
         		"file:src/main/resources/otherobjects.resources/config/applicationContext-dao.xml",};
     }
 	
-	public void testIndex()
-	{
-		IndexedPersistentObject ipo = new IndexedPersistentObject();
-		ipo.setTitle("A test Title");
-		ipo.setSummary("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris adipiscing porttitor neque. Fusce aliquet. Etiam euismod adipiscing sapien. Etiam consequat nisi sit amet velit. Nunc luctus. Nam blandit massa at est. Integer pellentesque. Curabitur ultrices tempus risus.");
-		ipo.setDate(new Date());
-		IndexedPersistentObject ipoNew = indexedPersistentObjectDao.save(ipo);
-		
-		System.out.println(ipoNew.getId());
-		
-	}
+//	public void testIndex()
+//	{
+//		IndexedPersistentObject ipo = new IndexedPersistentObject();
+//		ipo.setTitle("A test Title");
+//		ipo.setSummary("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris adipiscing porttitor neque. Fusce aliquet. Etiam euismod adipiscing sapien. Etiam consequat nisi sit amet velit. Nunc luctus. Nam blandit massa at est. Integer pellentesque. Curabitur ultrices tempus risus.");
+//		ipo.setDate(new Date());
+//		IndexedPersistentObject ipoNew = indexedPersistentObjectDao.save(ipo);
+//		
+//		System.out.println(ipoNew.getId());
+//		
+//	}
 }
