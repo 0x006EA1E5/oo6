f58341f89be2a695715dbcdc1edc1d0a8ea2c2ef
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-09-08 13:28:32 +0000

    Comment out Atom for now
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40464

--- src/main/java/org/otherobjects/cms/syndication/DynaNodeAtomHandler.java
+++ src/main/java/org/otherobjects/cms/syndication/DynaNodeAtomHandler.java
@@ -1,19 +1,19 @@
-package org.otherobjects.cms.syndication;
-
-import javax.servlet.http.HttpServletRequest;
-
-import com.sun.syndication.propono.atom.server.impl.FileBasedAtomHandler;
-
-public class DynaNodeAtomHandler extends FileBasedAtomHandler
-{
-    public DynaNodeAtomHandler(HttpServletRequest req)
-    {
-        super(req);
-    }
-
-    @Override
-    public boolean isCollectionURI(String[] pathInfo)
-    {
-        return super.isCollectionURI(pathInfo);
-    }
-}
+//package org.otherobjects.cms.syndication;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//import com.sun.syndication.propono.atom.server.impl.FileBasedAtomHandler;
+//
+//public class DynaNodeAtomHandler extends FileBasedAtomHandler
+//{
+//    public DynaNodeAtomHandler(HttpServletRequest req)
+//    {
+//        super(req);
+//    }
+//
+//    @Override
+//    public boolean isCollectionURI(String[] pathInfo)
+//    {
+//        return super.isCollectionURI(pathInfo);
+//    }
+//}
