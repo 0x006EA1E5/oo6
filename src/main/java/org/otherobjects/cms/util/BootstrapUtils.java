e662a27ef234ad255f36775e4d3d8e93c3a0a59e
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-09-24 10:51:38 +0000

    Updates
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40533

--- src/main/java/org/otherobjects/cms/util/BootstrapUtils.java
+++ src/main/java/org/otherobjects/cms/util/BootstrapUtils.java
@@ -21,6 +21,9 @@ import org.otherobjects.cms.model.Role;
 import org.otherobjects.cms.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.io.Resource;
 
 /**
@@ -32,7 +35,7 @@ import org.springframework.core.io.Resource;
  * @author rich
  */
 @SuppressWarnings("unchecked")
-public class BootstrapUtils
+public class BootstrapUtils implements ApplicationListener
 {
     private final Logger logger = LoggerFactory.getLogger(BootstrapUtils.class);
 
@@ -47,6 +50,29 @@ public class BootstrapUtils
     public void setDaoService(DaoService daoService)
     {
         this.daoService = daoService;
+    }
+
+    public void onApplicationEvent(ApplicationEvent e)
+    {
+        if (e instanceof ContextRefreshedEvent)
+        {
+            this.logger.info("**************************************************************");
+            this.logger.info("**************************************************************");
+            this.logger.info("  ___ _____ _   _ _____ ____       _     _           _       ");
+            this.logger.info(" / _ \\_   _| | | | ____|  _ \\ ___ | |__ (_) ___  ___| |_ ___ ");
+            this.logger.info("| | | || | | |_| |  _| | |_) / _ \\| '_ \\| |/ _ \\/ __| __/ __|");
+            this.logger.info("| |_| || | |  _  | |___|  _ < (_) | |_) | |  __/ (__| |_\\__ \\");
+            this.logger.info(" \\___/ |_| |_| |_|_____|_| \\_\\___/|_.__// |\\___|\\___|\\__|___/");
+            this.logger.info("                                      |__/                ");
+            this.logger.info("");
+            this.logger.info("******************* Started up successfully ******************");
+            this.logger.info("");
+            this.logger.info("Site  : http://localhost:8080/go/");
+            this.logger.info("Admin : http://localhost:8080/go/workbench/");
+            this.logger.info("");
+            this.logger.info("**************************************************************");
+            this.logger.info("**************************************************************");
+        }
     }
 
     /**
