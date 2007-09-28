f458a44ab76e04218004fc9c6815f286488f633d
joerg@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-09-28 09:49:10 +0000

    cleanup
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40572

--- src/main/java/org/otherobjects/cms/scheduler/QuartzSchedulerConfigurationBean.java
+++ src/main/java/org/otherobjects/cms/scheduler/QuartzSchedulerConfigurationBean.java
@@ -8,7 +8,6 @@ import org.otherobjects.cms.model.CmsNode;
 import org.otherobjects.cms.model.DynaNode;
 import org.otherobjects.cms.types.TypeService;
 import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEvent;
@@ -18,6 +17,9 @@ import org.springframework.context.event.ContextRefreshedEvent;
 /**
  * Simple bean that configures the built-in quartz scheduler by querying for all {@link PersistentJobDescription} objects under a certain
  * repository path. Configuration happens at application context startup. 
+ * Scheduled jobs are refreshed when their repository based {@link PersistentJobDescription} is published.
+ * 
+ * 
  * @author joerg
  *
  */
@@ -29,8 +31,35 @@ public class QuartzSchedulerConfigurationBean implements ApplicationListener
     private DynaNodeDao dynaNodeDao;
     private TypeService typeService;
 
-    private void configure()
+    /**
+     * listen to contextRefreshed and publish events to initialize / refresh jobs 
+     */
+    public void onApplicationEvent(ApplicationEvent event)
     {
+        //initialise when ctx is ready
+        if (event instanceof ContextRefreshedEvent && ((ContextRefreshedEvent)event).getApplicationContext().getParent() == null) // only do for root context refresh
+        {
+            init();
+        }
+        //refresh or add jobs when they are published
+        else if (event instanceof PublishEvent)
+        {
+            CmsNode cmsNode = ((PublishEvent) event).getCmsNode();
+            if (cmsNode instanceof PersistentJobDescription)
+            {
+                PersistentJobDescription jobDescription = (PersistentJobDescription) cmsNode;
+                scheduleJob(jobDescription);
+            }
+        }
+    }
+    
+    /**
+     * schedule all valid jobs from repository's /scheduler path
+     */
+    private void init()
+    {
+        typeService.getType(PersistentJobDescription.class.getName());
+        
         List<DynaNode> jobs = dynaNodeDao.getAllByPath("/scheduler");
         for (DynaNode node : jobs)
         {
@@ -42,59 +71,26 @@ public class QuartzSchedulerConfigurationBean implements ApplicationListener
             }
         }
     }
-
-    public void onApplicationEvent(ApplicationEvent event)
-    {
-        //initialise when ctx is ready
-        if (event instanceof ContextRefreshedEvent && ((ContextRefreshedEvent)event).getApplicationContext().getParent() == null) // only do for root context refresh
-        {
-            init();
-        }
-        //only interested in PublishEvents
-        else if (event instanceof PublishEvent)
-        {
-            CmsNode cmsNode = ((PublishEvent) event).getCmsNode();
-            if (cmsNode instanceof PersistentJobDescription)
-            {
-                PersistentJobDescription jobDescription = (PersistentJobDescription) cmsNode;
-                try
-                {
-                    unscheduleJob(jobDescription);
-                    scheduleJob(jobDescription);
-                }
-                catch (SchedulerException e)
-                {
-                    logger.error("Couldn't unschedule existing job - job was not rescheduled", e);
-                }
-            }
-        }
-    }
-
-    private void init()
-    {
-        typeService.getType(PersistentJobDescription.class.getName());
-        configure();
-    }
-
+    
+    /**
+     * Schedule given job, deleting any existing job with the same name beforehand.
+     * 
+     * @param jobDescription
+     */
     private void scheduleJob(PersistentJobDescription jobDescription)
     {
         try
         {
         	if(scheduler.deleteJob(jobDescription.getId(), PersistentJobDescription.JOB_GROUP_NAME))
         	{
-        		logger.info("Job with name " + jobDescription.getId() + " already existed and was therefore deleted before scheduling it again");
+        		logger.info("Job " + jobDescription.getLabel() + " [" + jobDescription.getId() + "] already existed and was therefore deleted before scheduling it again");
         	}
             scheduler.scheduleJob(jobDescription.getJobDetail(), jobDescription.getTrigger());
         }
         catch (Exception e)
         {
-            logger.warn("Couldn't schedule Job " + jobDescription.getLabel(), e);
+            logger.warn("Couldn't schedule Job " + jobDescription.getLabel() + " [" + jobDescription.getId() + "]", e);
         }
-    }
-
-    private void unscheduleJob(PersistentJobDescription jobDescription) throws SchedulerException
-    {
-        scheduler.deleteJob(jobDescription.getId(), PersistentJobDescription.JOB_GROUP_NAME);
     }
 
     public void setTypeService(TypeService typeService)
