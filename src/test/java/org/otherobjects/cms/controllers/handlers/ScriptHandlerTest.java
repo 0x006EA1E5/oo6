879472193ead186e126b77c0d94066df1f36ba95
joerg@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-10-05 10:23:07 +0000

    test added
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40598

---
+++ src/test/java/org/otherobjects/cms/controllers/handlers/ScriptHandlerTest.java
@@ -0,0 +1,69 @@
+package org.otherobjects.cms.controllers.handlers;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.io.FileUtils;
+import org.otherobjects.cms.model.DynaNode;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class ScriptHandlerTest extends TestCase {
+	
+	private boolean scriptDirExisted = false;
+	private File scriptFolder = new File("scripts");
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		if(!scriptFolder.isDirectory() && scriptFolder.exists())
+			throw new RuntimeException("Test can not be run if the path 'scripts' exists and is not a directory");
+		
+		if(scriptFolder.exists())
+			scriptDirExisted = true;
+		else
+			scriptFolder.mkdir();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		
+		if(!scriptDirExisted)
+			scriptFolder.delete();
+	}
+
+	public void testRunScript()
+	{
+		MockHttpServletRequest  request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		
+		File script = null;
+		try{
+			script = new File("scripts/test.groovy");
+			FileUtils.writeStringToFile(script, "response.addHeader('test', 'test');");
+			
+			request.addParameter("script", "test");
+			
+			ScriptHandler scriptHandler = new ScriptHandler();
+			scriptHandler.setDaoService(null);
+			
+			DynaNode dynaNode = new DynaNode();
+			
+			scriptHandler.handleRequest(dynaNode, request, response);
+			
+			assertEquals("test", response.getHeader("test"));
+			
+		} catch (Exception e) {
+			
+		}
+		finally
+		{
+			if(script != null)
+				script.delete();
+		}
+		
+	}
+}
