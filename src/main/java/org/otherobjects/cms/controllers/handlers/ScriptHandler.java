842da70f4d237fbf8ecc3d8084298bdcc3d8a6b6
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2007-10-05 14:27:47 +0000

    Update Script Handler
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40603

--- src/main/java/org/otherobjects/cms/controllers/handlers/ScriptHandler.java
+++ src/main/java/org/otherobjects/cms/controllers/handlers/ScriptHandler.java
@@ -10,37 +10,76 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.otherobjects.cms.OtherObjectsException;
 import org.otherobjects.cms.dao.DaoService;
 import org.otherobjects.cms.model.DynaNode;
+import org.otherobjects.cms.model.Script;
+import org.otherobjects.cms.model.ScriptResource;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.util.Assert;
 import org.springframework.web.servlet.ModelAndView;
 
-public class ScriptHandler implements ResourceHandler
+/**
+ * TODO Add multiple lanugaue support.
+ * 
+ * @author rich
+ *
+ */
+public class ScriptHandler implements ResourceHandler, ApplicationContextAware
 {
     private DaoService daoService;
+    private ApplicationContext applicationContext;
 
+    public ModelAndView handleRequest(DynaNode resourceObject, HttpServletRequest request, HttpServletResponse response) throws Exception
+    {
+        Assert.notNull(resourceObject, "resourceObject must not be null");
+        Assert.isInstanceOf(ScriptResource.class, resourceObject, "resourceObject must be a ScriptResource not: " + resourceObject.getClass().getName());
+
+        ScriptResource scriptResource = (ScriptResource) resourceObject;
+        Script script = scriptResource.getScript();
+
+        String code = null;
+        if (!StringUtils.isEmpty(script.getScriptCode()))
+        {
+            code = script.getScriptCode();
+        }
+        else if (!StringUtils.isEmpty(script.getScriptPath()))
+        {
+            code = FileUtils.readFileToString(new File(script.getScriptPath()));
+        }
+        else
+        {
+            // TODO Add waring if both are specified
+            // TODO Should this error be in Scipt.isValid() 
+            throw new OtherObjectsException("You must provide either script code or a scripth path in: " + script.getJcrPath());
+        }
+        
+        // Execute script
+        runScript(code, request, response);
+        return null;
+    }
+
+    protected void runScript(String code, HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        Binding binding = new Binding();
+        binding.setVariable("daoService", this.daoService);
+        binding.setVariable("request", request);
+        binding.setVariable("response", response);
+        binding.setVariable("applicationContext", applicationContext);
+        GroovyShell shell = new GroovyShell(binding);
+        shell.evaluate(code);
+    }
+    
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        this.applicationContext = applicationContext;
+    }
+    
     public void setDaoService(DaoService daoService)
     {
         this.daoService = daoService;
     }
-
-    public ModelAndView handleRequest(DynaNode resourceObject, HttpServletRequest request, HttpServletResponse response) throws Exception
-    {
-        if (request.getParameter("script") != null)
-        {
-            runScript(request, response);
-        }
-        return null;
-    }
-
-    protected void runScript(HttpServletRequest request, HttpServletResponse response) throws IOException
-    {
-        Binding binding = new Binding();
-        String script = FileUtils.readFileToString(new File("scripts/" + request.getParameter("script") + ".groovy"));
-        binding.setVariable("daoService", this.daoService);
-        binding.setVariable("request", request);
-        binding.setVariable("response", response);
-        GroovyShell shell = new GroovyShell(binding);
-        shell.evaluate(script);
-    }
-
 }
