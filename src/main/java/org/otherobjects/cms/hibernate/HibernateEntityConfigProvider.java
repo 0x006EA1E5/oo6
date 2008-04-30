bc4600dd9c7bd520c873c53a655a288828d3c400
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2008-04-30 16:41:44 +0000

    Fix warnings
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40792

--- src/main/java/org/otherobjects/cms/hibernate/HibernateEntityConfigProvider.java
+++ src/main/java/org/otherobjects/cms/hibernate/HibernateEntityConfigProvider.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Assert;
 
+@SuppressWarnings("unchecked")
 public class HibernateEntityConfigProvider implements InitializingBean
 {
     protected final Logger logger = LoggerFactory.getLogger(getClass());
