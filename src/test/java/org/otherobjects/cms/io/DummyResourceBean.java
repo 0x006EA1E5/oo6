fee711d2f4b949fe18ad548ff72761c61178243a
joerg@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2008-05-19 12:06:23 +0000

    add
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40823

---
+++ src/test/java/org/otherobjects/cms/io/DummyResourceBean.java
@@ -0,0 +1,16 @@
+package org.otherobjects.cms.io;
+
+public class DummyResourceBean
+{
+    private OoResource testfile;
+
+    public OoResource getTestfile()
+    {
+        return testfile;
+    }
+
+    public void setTestfile(OoResource testfile)
+    {
+        this.testfile = testfile;
+    }
+}
