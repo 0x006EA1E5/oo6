1eed225960453e8a4bb56d20bc3454c105ef01a1
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2008-05-22 14:23:10 +0000

    Update tests
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40861

--- src/main/java/org/otherobjects/cms/test/BaseDaoTestCase.java
+++ src/main/java/org/otherobjects/cms/test/BaseDaoTestCase.java
@@ -1,37 +1,96 @@
 package org.otherobjects.cms.test;
 
-import java.util.ResourceBundle;
+import java.io.InputStream;
+import java.net.URL;
+import java.sql.Connection;
 
+import org.dbunit.DefaultDatabaseTester;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.XmlDataSet;
+import org.dbunit.ext.h2.H2DataTypeFactory;
+import org.dbunit.operation.DatabaseOperation;
 import org.hibernate.SessionFactory;
+import org.otherobjects.cms.bootstrap.DbSchemaInitialiser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.HibernateTemplate;
-import org.springframework.test.AbstractTransactionalDataSourceSpringContextTests;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit38.AbstractTransactionalJUnit38SpringContextTests;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
-/**
- * Base class for running DAO tests.
- * @author mraible
- */
-public abstract class BaseDaoTestCase extends AbstractTransactionalDataSourceSpringContextTests {
-	protected final Logger logger = LoggerFactory.getLogger(getClass());
-    protected ResourceBundle rb;
+@ContextConfiguration(locations = {"classpath:org/otherobjects/cms/bootstrap/db-bootstrap-context.xml"})
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@Transactional
+public abstract class BaseDaoTestCase extends AbstractTransactionalJUnit38SpringContextTests
+{
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
 
-    protected String[] getConfigLocations() {
-        setAutowireMode(AUTOWIRE_BY_NAME);
-        return new String[] {
-                "file:src/test/resources/applicationContext-resources.xml",
-                "file:src/main/resources/otherobjects.resources/config/applicationContext-dao.xml",
-            };
-    }
-
+    @Autowired
+    protected SessionFactory sessionFactory;
+    
+    @Autowired
+    DbSchemaInitialiser dbSchemaInitialiser;
+    
     /**
      * Create a HibernateTemplate from the SessionFactory and call flush() and clear() on it.
      * Designed to be used after "save" methods in tests: http://issues.appfuse.org/browse/APF-178.
      */
-    protected void flush() {
-        HibernateTemplate hibernateTemplate =
-                new HibernateTemplate((SessionFactory) applicationContext.getBean("sessionFactory"));
+    protected void flush()
+    {
+        HibernateTemplate hibernateTemplate = new HibernateTemplate((SessionFactory) applicationContext.getBean("sessionFactory"));
         hibernateTemplate.flush();
         hibernateTemplate.clear();
+    }
+
+    @SuppressWarnings("deprecation")
+    protected void loadSeedData(String seedDataFile) throws Exception
+    {
+        // FIXME This only should happen once
+        dbSchemaInitialiser.initialise(true);
+        
+        logger.debug("Loding seed data: {}", seedDataFile);
+        IDatabaseConnection dbunitConn = null;
+
+        try
+        {
+            Connection conn = sessionFactory.openSession().connection();
+            conn.setAutoCommit(true);
+            dbunitConn = new DatabaseConnection(conn);
+            DatabaseConfig config = dbunitConn.getConfig();
+            config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new H2DataTypeFactory());
+
+            IDataSet dataSet = loadDataSet(seedDataFile);
+            DefaultDatabaseTester databaseTester = new DefaultDatabaseTester(dbunitConn);
+            databaseTester.setDataSet(dataSet);
+            databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+            databaseTester.onSetup();
+        }
+        catch (Exception ex)
+        {
+            logger.debug("Error seeding database", ex);
+            throw ex;
+        }
+        finally
+        {
+            if (dbunitConn != null)
+            {
+                dbunitConn.close();
+            }
+        }
+    }
+
+    private IDataSet loadDataSet(String path) throws Exception
+    {
+        ClassLoader cl = this.getClass().getClassLoader();
+        URL resource = cl.getResource(path);
+        assertNotNull("Seed data file not found in classpath: " + path, resource);
+        InputStream stream = resource.openStream();
+        IDataSet data = new XmlDataSet(stream);
+        return data;
     }
 }
