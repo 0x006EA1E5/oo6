8dfd1e00f2c8eeae10918ae58aa8dfd04615807f
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2008-06-06 10:57:53 +0000

    Update docs
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40960

--- src/main/java/org/otherobjects/cms/controllers/TestController.java
+++ src/main/java/org/otherobjects/cms/controllers/TestController.java
@@ -1,5 +1,9 @@
 package org.otherobjects.cms.controllers;
 
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import javax.annotation.Resource;
@@ -8,8 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
-import org.otherobjects.cms.io.OoResource;
+import org.otherobjects.cms.dao.DaoService;
 import org.otherobjects.cms.io.OoResourceLoader;
+import org.otherobjects.cms.types.TypeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
@@ -17,11 +22,11 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 @Controller
 public class TestController extends MultiActionController
 {
-//    @Resource
-//    private DaoService daoService;
-//
-//    @Resource
-//    private TypeService typeService;
+    @Resource
+    private DaoService daoService;
+
+    @Resource
+    private TypeService typeService;
 
     @Resource
     private OoResourceLoader ooResourceLoader;
@@ -32,19 +37,19 @@ public class TestController extends MultiActionController
         System.err.println("TEST");
 
         // Load data file
-//        logger.debug("Running setup scripts.");
-//        Binding binding = new Binding();
-//        binding.setProperty("daoService", daoService);
-//        binding.setProperty("typeService", typeService);
-//        GroovyShell shell = new GroovyShell(binding);
-//        String script = IOUtils.toString(new FileInputStream("data-transfer/import.groovy"));
-//        shell.evaluate(script);
+        logger.debug("Running setup scripts.");
+        Binding binding = new Binding();
+        binding.setProperty("daoService", daoService);
+        binding.setProperty("typeService", typeService);
+        GroovyShell shell = new GroovyShell(binding);
+        String script = IOUtils.toString(new FileInputStream("data-transfer/import.groovy"));
+        shell.evaluate(script);
 
         
-        OoResource resource = ooResourceLoader.getResource("/site/templates/blocks/client.ftl");
-
-        String string = IOUtils.toString(resource.getInputStream());
-        
+//        OoResource resource = ooResourceLoader.getResource("/site/templates/blocks/client.ftl");
+//
+//        String string = IOUtils.toString(resource.getInputStream());
+//        
         
         return null;
     }
