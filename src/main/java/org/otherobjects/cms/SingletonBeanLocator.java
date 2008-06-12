75993d9751832d97ec2e0ded3d2f4a84e459919f
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2008-06-12 14:56:37 +0000

    Add methods to aid testing
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%40985

--- src/main/java/org/otherobjects/cms/SingletonBeanLocator.java
+++ src/main/java/org/otherobjects/cms/SingletonBeanLocator.java
@@ -1,5 +1,8 @@
 package org.otherobjects.cms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.util.Assert;
@@ -7,7 +10,8 @@ import org.springframework.util.Assert;
 public class SingletonBeanLocator implements BeanFactoryAware
 {
     private static BeanFactory beanFactory;
-
+    private static Map<String,Object> overrides = new HashMap<String,Object>();
+    
     public void setBeanFactory(BeanFactory beanFactory)
     {
         SingletonBeanLocator.beanFactory = beanFactory;
@@ -20,8 +24,15 @@ public class SingletonBeanLocator implements BeanFactoryAware
 
     public static Object getBean(String name)
     {
+        if(overrides.containsKey(name))
+            return overrides.get(name);
+
         Assert.notNull(beanFactory, "BeanFactory not initialised.");
         return beanFactory.getBean(name);
     }
-
+    
+    public static void registerTestBean(String name, Object bean)
+    {
+        overrides.put(name, bean);
+    }
 }
