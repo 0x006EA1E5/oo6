53d15bc13ac28ef2e27b036099c22184e79769ac
joerg@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2008-06-16 09:52:28 +0000

    added collectionElementType prop for blocks property
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%401000

--- src/main/java/org/otherobjects/cms/model/TemplateRegion.java
+++ src/main/java/org/otherobjects/cms/model/TemplateRegion.java
@@ -3,6 +3,7 @@ package org.otherobjects.cms.model;
 import java.util.List;
 
 import org.otherobjects.cms.types.annotation.Property;
+import org.otherobjects.cms.types.annotation.PropertyType;
 import org.otherobjects.cms.types.annotation.Type;
 
 @Type
@@ -12,29 +13,33 @@ public class TemplateRegion extends BaseNode
     private String label;
     private List<TemplateBlock> blocks;
 
+    @Override
     @Property(order = 10)
     public String getCode()
     {
         return code;
     }
 
+    @Override
     public void setCode(String code)
     {
         this.code = code;
     }
 
+    @Override
     @Property(order = 20)
     public String getLabel()
     {
         return label;
     }
 
+    @Override
     public void setLabel(String label)
     {
         this.label = label;
     }
 
-    @Property(order = 30)
+    @Property(order = 30, collectionElementType = PropertyType.REFERENCE)
     public List<TemplateBlock> getBlocks()
     {
         return blocks;
