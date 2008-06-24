16742ad4d11466374c8481ced2785c0232346e29
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2008-06-24 21:08:13 +0000

    Add txt files
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%401054

--- docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mvn jdocbook:resources
-mvn -Ddocbook.name=user-guide jdocbook:generate
-mvn -Ddocbook.name=designer-guide jdocbook:generate
-mvn -Ddocbook.name=developer-guide jdocbook:generate
-mvn -Ddocbook.name=hosting-guide jdocbook:generate
-
-
-
