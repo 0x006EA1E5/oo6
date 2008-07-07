75db9f5f47ae0a79ee68454a754b27d3e2aca03a
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2008-07-07 08:27:36 +0000

    Preprae for DC metadata
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%401118

---
+++ src/main/java/org/otherobjects/cms/model/DublinCoreMetaData.java
@@ -0,0 +1,71 @@
+package org.otherobjects.cms.model;
+
+import java.util.List;
+
+import org.otherobjects.cms.types.annotation.Property;
+import org.otherobjects.cms.types.annotation.Type;
+
+/**
+ * Title
+Creator
+Subject
+Description
+Publisher
+Contributor
+Date
+Type
+Format
+Identifier
+Source
+Language
+Relation
+Coverage
+Rights
+ */
+@Type
+public class DublinCoreMetaData extends BaseNode
+{
+    private String title;
+    private String description;
+    private List<String> keywords;
+
+    @Override
+    public String getCode()
+    {
+        return "metaData";
+    }
+
+    @Property(order = 10)
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    @Property(order = 20)
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    @Property(order = 30)
+    public List<String> getKeywords()
+    {
+        return keywords;
+    }
+
+    public void setKeywords(List<String> keywords)
+    {
+        this.keywords = keywords;
+    }
+
+}
