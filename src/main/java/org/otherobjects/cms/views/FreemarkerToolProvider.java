780dfd3ddc744bef25776425db37689475435160
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2008-12-05 09:30:13 +0000

    Tool discovery updates
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%401454

--- src/main/java/org/otherobjects/cms/views/FreemarkerToolProvider.java
+++ src/main/java/org/otherobjects/cms/views/FreemarkerToolProvider.java
@@ -1,29 +1,30 @@
 package org.otherobjects.cms.views;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.otherobjects.cms.config.OtherObjectsConfigurator;
 import org.otherobjects.cms.discovery.AnnotatedClassesScanner;
-import org.otherobjects.cms.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.util.Assert;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 @SuppressWarnings("unchecked")
-public class FreemarkerToolProvider implements InitializingBean
+public class FreemarkerToolProvider implements InitializingBean, ApplicationContextAware
 {
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     private AnnotatedClassesScanner scanner;
     private OtherObjectsConfigurator otherObjectsConfigurator;
+    private ApplicationContext applicationContext;
 
     private Set<String> annotatedClasses;
     private String[] annotatedPackages;
+
 
     public void setScanner(AnnotatedClassesScanner scanner)
     {
@@ -32,35 +33,52 @@ public class FreemarkerToolProvider implements InitializingBean
 
     public void afterPropertiesSet() throws Exception
     {
-        Assert.notNull(scanner, "ClassPath scanner must be set");
-        Assert.notNull(otherObjectsConfigurator, "OtherObjectsConfigurator must be set");
-        List<String> packages = new ArrayList<String>();
-        packages.add(otherObjectsConfigurator.getProperty("site.tool.packages"));
-        packages.add(otherObjectsConfigurator.getProperty("otherobjects.tool.packages"));
-
-        annotatedPackages = StringUtils.join(packages, ',').split(",");
-
-        logger.info("Scanning the following packages: " + StringUtils.join(annotatedPackages, ','));
-
-        annotatedClasses = scanner.findAnnotatedClasses(annotatedPackages, Tool.class);
-
-        logger.info("Found the following tool entities: " + StringUtils.join(annotatedClasses, ','));
+//        Assert.notNull(scanner, "ClassPath scanner must be set");
+//        Assert.notNull(otherObjectsConfigurator, "OtherObjectsConfigurator must be set");
+//        List<String> packages = new ArrayList<String>();
+//        packages.add(otherObjectsConfigurator.getProperty("site.tool.packages"));
+//        packages.add(otherObjectsConfigurator.getProperty("otherobjects.tool.packages"));
+//
+//        annotatedPackages = StringUtils.join(packages, ',').split(",");
+//
+//        logger.info("Scanning the following packages: " + StringUtils.join(annotatedPackages, ','));
+//
+//        annotatedClasses = scanner.findAnnotatedClasses(annotatedPackages, Tool.class);
+//
+//        logger.info("Found the following tool entities: " + StringUtils.join(annotatedClasses, ','));
+        getTools();
     }
 
     public Map<String, Object> getTools()
     {
+//        Map<String, Object> tools = new HashMap<String, Object>();
+//        for (String className : annotatedClasses)
+//        {
+//            try
+//            {
+//                Class toolClass = (Class<Tool>) Class.forName(className);
+//                Tool annotation = (Tool) toolClass.getAnnotation(Tool.class);
+//                tools.put(annotation.value(), toolClass.newInstance());
+//            }
+//            catch (Exception e)
+//            {
+//                logger.warn("Could not create tool className: " + className, e);
+//            }
+//        }
+//        return tools;
         Map<String, Object> tools = new HashMap<String, Object>();
-        for (String className : annotatedClasses)
+        for (String beanName : applicationContext.getBeanDefinitionNames())
         {
             try
             {
-                Class toolClass = (Class<Tool>) Class.forName(className);
-                Tool annotation = (Tool) toolClass.getAnnotation(Tool.class);
-                tools.put(annotation.value(), toolClass.newInstance());
+                Object tool = applicationContext.getBean(beanName);
+                Tool annotation = (Tool) tool.getClass().getAnnotation(Tool.class);
+                if(annotation!=null)
+                    tools.put(annotation.value(), tool);
             }
             catch (Exception e)
             {
-                logger.warn("Could not create tool className: " + className, e);
+                logger.warn("Error scanning bean for @Tool: " + beanName, e);
             }
         }
         return tools;
@@ -76,4 +94,8 @@ public class FreemarkerToolProvider implements InitializingBean
         this.otherObjectsConfigurator = otherObjectsConfigurator;
     }
 
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        this.applicationContext = applicationContext;
+    }
 }
