6b0237fc33adf90a27c82f44adbc776a4bf2bd77
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2008-12-05 17:03:47 +0000

    Add support for FormTool
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%401459

--- src/main/java/org/otherobjects/cms/binding/OORequestContext.java
+++ src/main/java/org/otherobjects/cms/binding/OORequestContext.java
@@ -9,17 +9,24 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.support.BindStatus;
 import org.springframework.web.servlet.support.RequestContext;
 
+@SuppressWarnings("unchecked")
 public class OORequestContext extends RequestContext
 {
+    private final Map model;
+    private final HttpServletRequest request;
+
     public OORequestContext(HttpServletRequest request, ModelMap model)
     {
         super(request, model);
+        this.request = request;
+        this.model = model;
     }
-    
-    @SuppressWarnings("unchecked")
+
     public OORequestContext(HttpServletRequest request, ServletContext servletContext, Map model)
     {
         super(request, servletContext, model);
+        this.request = request;
+        this.model = model;
     }
 
     @Override
@@ -28,5 +35,27 @@ public class OORequestContext extends RequestContext
         //path.replaceAll("resourceObject\\.", "");
         //path = "resourceObject.data["+path+"]";
         return new BindStatus(this, path, isDefaultHtmlEscape());
+    }
+
+    /**
+     * Override this method to allow BindStatus to look in request attributes as well as the model.
+     * This is essential since FormTool currently inserts form objects into the request attributes not
+     * the model. 
+     */
+    @Override
+    protected Object getModelObject(String modelName)
+    {
+        Object o = null;
+        // Look in model first
+        if (this.model != null)
+        {
+            o = this.model.get(modelName);
+        }
+        // Then try again in request attributes
+        if(o==null)
+        {
+            o = this.request.getAttribute(modelName);
+        }
+        return o;
     }
 }
