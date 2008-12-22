66c02df9134cb5b248f04bbab9226cadc1072ba3
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2008-12-22 19:31:47 +0000

    Rename file
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%401593

---
+++ src/main/java/org/otherobjects/cms/views/FreeMarkerToolProvider.java
@@ -0,0 +1,61 @@
+package org.otherobjects.cms.views;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.util.Assert;
+
+/**
+ * Scans applicationContext for @Tool annotated components.
+ * 
+ * @author rich
+ */
+public class FreeMarkerToolProvider implements ApplicationContextAware
+{
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private ApplicationContext applicationContext;
+
+    public Map<String, Object> getTools()
+    {
+        Map<String, Object> tools = new HashMap<String, Object>();
+        for (String beanName : this.applicationContext.getBeanDefinitionNames())
+        {
+            try
+            {
+                Object tool = this.applicationContext.getBean(beanName);
+                Tool annotation = tool.getClass().getAnnotation(Tool.class);
+                String name = StringUtils.isNotEmpty(annotation.value()) ? annotation.value() : generateName(tool.getClass().getName());
+                if (annotation != null)
+                {
+                    tools.put(name, tool);
+                }
+            }
+            catch (Exception e)
+            {
+                //this.logger.warn("Error scanning bean for @Tool: " + beanName, e);
+            }
+        }
+        return tools;
+    }
+
+    protected String generateName(String name)
+    {
+        Assert.notNull(name, "Can not generate name from null string.");
+        // Strip off package
+        name = StringUtils.substringAfterLast(name, ".");
+        // Make first letter lowercase
+        return name.substring(0, 1).toLowerCase() + name.substring(1);
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        this.applicationContext = applicationContext;
+    }
+}
