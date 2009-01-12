fa6e643311da4bedd65d3b8712c7caa4bd8f536a
rich@89b2ea68-c832-0410-a685-f4ea58b14c71 <none@none>
2009-01-12 18:14:50 +0000

    More form support
    --HG--
    extra : convert_revision : svn%3A89b2ea68-c832-0410-a685-f4ea58b14c71/otherobjects/trunk%401659

--- src/main/java/org/otherobjects/cms/tools/FormTool.java
+++ src/main/java/org/otherobjects/cms/tools/FormTool.java
@@ -44,5 +44,40 @@ public class FormTool
         this.request.setAttribute(attributeName, Class.forName(typeName).newInstance());
         return null;
     }
+    
+    public void unregisterFormObject(String attributeName, String typeName) throws InstantiationException, IllegalAccessException, ClassNotFoundException
+    {
+        this.request.removeAttribute(attributeName + "TypeDef");
+        this.request.removeAttribute(BindingResult.MODEL_KEY_PREFIX + attributeName);
+        HttpSession session = request.getSession(false);
+        if (session != null)
+        {
+            session.removeAttribute(BindingResult.MODEL_KEY_PREFIX + attributeName);
+        }
 
+    }
+
+    /**
+     * 
+     * FIXME Not sure this is a good place
+     * @param attributeName
+     * @param typeName
+     * @return
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws ClassNotFoundException
+     */
+    public boolean hasErrors(String attributeName, String typeName) throws InstantiationException, IllegalAccessException, ClassNotFoundException
+    {
+        boolean hasErrors = false;
+        HttpSession session = request.getSession(false);
+        if (session != null)
+        {
+            BindingResult errors = (BindingResult) session.getAttribute(BindingResult.MODEL_KEY_PREFIX + attributeName);
+            if(errors!=null)
+                hasErrors = errors.hasErrors();
+        }
+        return hasErrors;
+    }
+    
 }
