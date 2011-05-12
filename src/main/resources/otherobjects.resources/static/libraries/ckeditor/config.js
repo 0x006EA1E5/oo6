cae3c6a725f323d4c64935bd86c01d6662fd3b36
Greg Eales <greg.eales@othermedia.com>
2011-05-12 17:27:12 +0100

    + ckEditor

---
+++ src/main/resources/otherobjects.resources/static/libraries/ckeditor/config.js
@@ -0,0 +1,31 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	
+	config.toolbar = 'OO';
+	
+	config.toolbar_OO =
+		[
+		    ['Source','-','Save','NewPage','Preview','-','Templates'],
+		    ['Cut','Copy','Paste','PasteText','PasteFromWord','-','SpellChecker', 'Scayt'],
+		    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+		    
+		    '/',
+		    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+		    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+		    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		    ['Link','Unlink','Anchor'],
+		    ['Image','Flash','Table','HorizontalRule','SpecialChar'],
+		    '/',
+		    ['Styles','Format','Font','FontSize'],
+		    ['Maximize', 'ShowBlocks','-','About']
+	];
+	
+};
